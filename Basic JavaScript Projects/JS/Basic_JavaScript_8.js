9bd2b6d18dc9a733ec1490196ceb19edf126c127
spookyinfo <53101711+spookyinfo@users.noreply.github.com>
2020-01-15 18:33:52 -0800

    Add files via upload

--- Basic JavaScript Projects/JS/Basic_JavaScript_8.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_8.js
@@ -1,5 +1,13 @@
-function ValueString() {
-    var str = "I don't.";
-    var res = str.valueOf();
-    document.getElementById("Value").innerHTML = res;
+function countdown() {
+    var seconds = document.getElementById("seconds").nodeValue;
+
+    function tick(){
+        second = seconds - 1;
+        timer.innerHTML = seconds;
+        setTimeout(tick, 1000);
+    if(seconds == -1) {
+        alert("Time's up!");
+    }
+        }
+    tick();
 }
