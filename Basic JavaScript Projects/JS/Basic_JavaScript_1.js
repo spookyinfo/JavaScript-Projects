da11dc7ff6f2073ef18f24e0a17012ed4aafefa7
spookyinfo <53101711+spookyinfo@users.noreply.github.com>
2020-01-22 16:29:22 -0800

    Add files via upload

--- Basic JavaScript Projects/JS/Basic_JavaScript_1.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -1,2 +1,42 @@
-var x = 25;
-document.write(x+30);
+window.alert("This is step one, now onto step two!")
+
+document.write("But this line is the result of step two. ")
+
+document.write("<br><br>")
+
+var x="And this line is a variable, the result of step three. "
+document.write(x + "\n")
+
+var y="(Just kidding. This is actually step four.)"+"\n"
+window.alert(y)
+
+document.write("<br><br>")
+
+document.write("And you could say, \"This line is step five, which makes use of the backslash,\" and you wouldn\'t be wrong.")
+
+document.write("<br><br>")
+
+document.write("Whereas this " + "very normal " + "looking line " + "is actually " + "a ridiculously " + "concatenated one, " + "for step six.")
+
+document.write("<br><br>")
+
+var a="Step seven " + "is a variable " + "that has also been " + "concatenated."
+document.write(a)
+
+document.write("<br><br>")
+
+var Steps = Eight="This is step eight, one of several variables.", Nine="Step nine assigns colors to all those variables (this one being pink).", Ten="This one's step ten.",  Eleven="Yep, step eleven here."
+document.write(Eight) 
+
+var Nine=Nine.fontcolor("pink");
+var Ten=Ten.fontcolor("purple");
+var Eight=Eight.fontcolor("black");
+var Eleven=Eleven.fontcolor("blue");
+
+document.write("<br><br>")
+
+document.write(Nine)
+
+document.write("<br><br>")
+
+document.write((250*250) + "  is the result of step ten, expressing the square of 250.")
--- Basic JavaScript Projects/JS/Basic_JavaScript_10_a.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_10_a.js
@@ -3,7 +3,7 @@ let cat = {
     color: "black",
     birthday: "2017",
     description : function() {
-        return "Shadow is a " + this.color + this.breed + this.birthday;
+        return "Shadow is a " + this.color + this.breed + " born in " + this.birthday; //This describes the full object 
     
     }
 }; 
